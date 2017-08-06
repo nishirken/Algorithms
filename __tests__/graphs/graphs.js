284bec1a88e66c44c63f787fc12972c96bdb37aa
Dmitrii Skurikhin <dskurikhin@devvela.com>
2017-08-06 23:32:37 +0500

    completed Dijkstra shortest way

--- __tests__/graphs/graphs.js
+++ __tests__/graphs/graphs.js
@@ -1,4 +1,6 @@
-const { createInDegree, createNext, createLinearOrdering } = require('../../graphs/topological-sort');
+const { createInDegree, createNext, createLinearOrdering } =
+  require('../../graphs/topological-sort');
+const { relax, searchMinShortest, dukstraSearch } = require('../../graphs/dukstra-sort');
 
 describe('Graphs', () => {
   describe('Topological sort', () => {
@@ -33,6 +35,57 @@ describe('Graphs', () => {
       const expectedOrder = [0, 1, 2, 3, 5, 6, 7, 4, 8, 9];
 
       expect(createLinearOrdering(graph, expectedNext(), expectedInDegree())).toEqual(expectedOrder);
+    });
+  });
+
+  describe('Search shortest way', () => {
+    test('Relax function', () => {
+      const graph = [
+        { 1: 1, 2: 1 },
+        { 3: 3, 4: 2 },
+        {},
+        {},
+        {},
+      ];
+      const shortest = [0, Infinity, Infinity, Infinity, Infinity];
+      const prev = [null, null, null, null, null];
+      const shortestAfterRelax = [0, 1, Infinity, Infinity, Infinity];
+      const prevAfterRelax = [null, 0, null, null, null];
+
+      relax(0, 1, shortest, prev, graph);
+      expect(shortest).toEqual(shortestAfterRelax);
+      expect(prev).toEqual(prevAfterRelax);
+    });
+
+    test('searchMinShortest function', () => {
+      const shortest = [0, 1, 1, 4, 3];
+      const Q = [
+        undefined,
+        { 3: 3, 4: 2 },
+        {},
+        {},
+        {},
+      ];
+
+      expect(searchMinShortest(shortest, Q)).toBe(1);
+    });
+
+    test('Dijkstra search', () => {
+      const graph = [
+        { 1: 1, 2: 1 },
+        { 3: 3, 4: 2 },
+        {},
+        {},
+        {},
+      ];
+      const initialShortest = [];
+      const initialPrev = [];
+      const expectedShortest = [0, 1, 1, 4, 3];
+      const expectedPrev = [null, 0, 0, 1, 1];
+
+      dukstraSearch(initialShortest, initialPrev, graph);
+      expect(initialShortest).toEqual(expectedShortest);
+      expect(initialPrev).toEqual(expectedPrev);
     });
   });
 });
